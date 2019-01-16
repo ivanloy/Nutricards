b1f2a67965a6b665e577af8d99c8f40669a2585b
Ignacio Van Loy <nachovanle@gmail.com>
2019-01-16 10:24:17 +0100

    Dairy and fish/meat points started

--- app/src/main/java/com/ivanloy/nutricards/controllers/PointsCalculator.kt
+++ app/src/main/java/com/ivanloy/nutricards/controllers/PointsCalculator.kt
@@ -17,7 +17,21 @@ class PointsCalculator {
             var total : Int = 0
 
             return total
+            //TODO Vegetable and cereal
+        }
 
+        fun calculateDairyPoints(amount : Int){
+            return amount
+        }
+
+        fun calculateMeatAndFishPoints(meatAmount : Int, fishAmount : Int){
+            var ret : Int = 0;
+            if(meatAmount >= 3) ret -= 4
+            if(fishAmount >= 3) ret -= 4
+            if(meatAmount < 3 && fishAmount < 3){ //TODO exclusive, legibility
+                if(meatAmount == 2 && fishAmount == 2) ret += 12
+                else ret += 6
+            }
         }
 
         fun getCardsMap(hand : Hand<FoodCard>) : HashMap<FoodCardTypes, Int>{
